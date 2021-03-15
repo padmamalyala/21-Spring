f585e29650d1ac09c7b7c5e89c3caa1970ae65b1
Padma Malyala <padmamalyala98@gmail.com>
2021-03-15 10:41:29 +0530

    project intial

---
+++ src/main/java/company/Spring/App.java
@@ -0,0 +1,13 @@
+package company.Spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
